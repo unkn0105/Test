53b57f418a265a78e3e656d1d8b3f9415e5b8af6
unkn0105 <ijs92227585@gmail.com>
2023-01-07 11:20:47 +0900

    initial

---
+++ sketch.js
@@ -0,0 +1,28 @@
+let ball = {
+    x: 300,
+    y: 200,
+    xspeed: 4,
+    yspeed: -3
+}
+
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(ball.x, ball.y, 24, 24);
+
+    if (ball.x > width || ball.x < 0) {
+        ball.xspeed = ball.xspeed * (-1);
+    }
+    if (ball.y > height || ball.y < 0) {
+        ball.yspeed = ball.yspeed * (-1);
+    }
+    ball.x = ball.x + ball.xspeed;
+    ball.y = ball.y + ball.yspeed;
+}
